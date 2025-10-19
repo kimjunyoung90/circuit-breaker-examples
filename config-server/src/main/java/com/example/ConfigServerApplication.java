149ee90d9dbdd75c4afe969fa79da30e829ade1f
junyoungKim <wnsdud1900427@gmail.com>
2025-10-19 14:17:11 +0900

    feat: config-server 모듈 추가

---
+++ config-server/src/main/java/com/example/ConfigServerApplication.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer //config server로 설정
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
